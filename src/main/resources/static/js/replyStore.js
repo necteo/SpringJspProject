cfbdd89d5e580aad5fa2f5ed62bb60eaa51f010c
necteo <89801040+necteo@users.noreply.github.com>
2025-12-30 14:41:49 +0900

    Dockerfile

---
+++ src/main/resources/static/js/replyStore.js
@@ -0,0 +1,14 @@
+const { defineStore } = Pinia
+// replyStore
+// vuex => pinia
+const useReplyStore = defineStore('reply', {
+	// state
+	state: () => {
+		
+	},
+	// getters
+	// actions
+	actions: {
+		
+	}
+})
